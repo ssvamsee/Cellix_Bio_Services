9c756b5d0dfbe807fea48ee79f880daa759dc1cd
Harshamasina <18uec021@lnmiit.ac.in>
2023-02-17 16:56:29 +0530

    changed

--- db/Conn.js
+++ db/Conn.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const DB = 'mongodb+srv://root:root@cellixbio.wa5aa69.mongodb.net/Cellix-Bio-Asests?retryWrites=true&w=majority'
 const DB = process.env.MONGODB_URI;
 
 mongoose.connect(DB, {
