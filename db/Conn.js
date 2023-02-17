9f8c9526f456b2c6ab33bb38752eb3d52f01a496
Harshamasina <18uec021@lnmiit.ac.in>
2023-02-17 16:41:24 +0530

    changed

--- db/Conn.js
+++ db/Conn.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const DB = 'mongodb+srv://root:root@cellixbio.wa5aa69.mongodb.net/Cellix-Bio-Asests?retryWrites=true&w=majority'
+// const DB = 'mongodb+srv://root:root@cellixbio.wa5aa69.mongodb.net/Cellix-Bio-Asests?retryWrites=true&w=majority'
+const DB = process.env.MONGODB_URI;
 
 mongoose.connect(DB, {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true,
-    // useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
     console.log('DB is Successfully Connected');
 }).catch((err) => console.log('No Connection'))
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('./db/Conn')
 app.use(cors());
 app.use(express.json());
+require('./db/Conn');
 app.use(require('./Router/auth'));
 
 app.listen(3003, () => {
     console.log(`Server is running at 3003 and also in AWS`);
-    console.log(`http://localhost:3003`, `http://13.232.172.223:3003/`);
+    console.log(`http://localhost:3003`);
 })
