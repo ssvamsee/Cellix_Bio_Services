7cc4ee3579045906b86b276c8611455a8fdcc6b1
Harshamasina <18uec021@lnmiit.ac.in>
2023-02-17 16:37:31 +0530

    changed

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const express = require('express');
-// const dotenv = require('dotenv');
 const app = express();
 const cors = require('cors');
 require('./db/Conn')
-
-// app.use(cors({
-//     origin: ["http://localhost:3000", "https://ssvamsee.github.io/Cellix_Bio/", "http://3.108.252.142/"],
-//     methods: ["GET", "POST"],
-// }))
-
 app.use(cors());
-
 app.use(express.json());
-
 app.use(require('./Router/auth'));
-
-
-const middleware = (req,res,next) => {
-    console.log(`Hello Middleware`);
-    next();
-}
 
 app.listen(3003, () => {
     console.log(`Server is running at 3003 and also in AWS`);
